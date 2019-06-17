9c108a35d1a6fe4718e9063e545d2d31dc900406
suman-github <47244645+suman-github@users.noreply.github.com>
2019-06-18 00:46:10 +0530

    Initial Version

---
+++ NumberToWordsConverter/NumberToWordsConverter/ConvertNumberToWords.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWordsConverter
+{
+    public class ConvertNumberToWords
+    {
+        public static string ConvertNumbertoWords(int number)
+        {   
+            string words = "";
+            if ((number / 1000000) > 0)
+            {
+                words += ConvertNumbertoWords(number / 1000000) + " MILLION ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += ConvertNumbertoWords(number / 1000) + " THOUSAND ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += ConvertNumbertoWords(number / 100) + " HUNDRED ";
+                number %= 100;
+            }
+            if (number > 0)
+            {
+                var unitDigits = new[]
+                {
+            "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN"
+        };
+                var tenDigits = new[]
+                {
+            "ZERO", "TEN", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY"
+        };
+                if (number < 20)
+                {
+                    words += unitDigits[number];
+                }
+                else
+                {
+                    words += tenDigits[number / 10];
+                    if ((number % 10) > 0) words += " " + unitDigits[number % 10];
+                }
+            }
+            return words;
+        }
+    }
+}
---
+++ NumberToWordsConverter/NumberToWordsConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWordsConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isNegative = "";
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter a Number to convert to words");
+                    string input = Console.ReadLine();
+                    if(input == "exit") break;
+                    int number = Convert.ToInt32(input);
+
+                    if (number == 0)
+                    {
+                        Console.WriteLine("ZERO");
+                    }
+                    else if (number < 0)
+                    {
+                        Console.WriteLine("MINUS " + ConvertNumberToWords.ConvertNumbertoWords(Math.Abs(number)));
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}", isNegative + ConvertNumberToWords.ConvertNumbertoWords(number));
+                    }
+                }
+                //Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
